e935ab388520fa69d3b0d1791e9b6081f9d89058
Armin Schrenk <armin.schrenk@skymatic.de>
2024-09-23 14:49:05 +0200

    modularize app

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+open module org.cryptomator.cli {
+    requires org.cryptomator.cryptofs;
+    requires org.cryptomator.frontend.fuse;
+    requires info.picocli;
+    requires org.slf4j;
+    requires org.slf4j.jul;
+    requires org.cryptomator.integrations.api;
+}
