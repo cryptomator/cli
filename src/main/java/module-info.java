883359cbe209559987afe5c92c0e5444e807c0b7
Armin Schrenk <armin.schrenk@skymatic.de>
2024-09-25 12:11:06 +0200

    colorize console

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,6 @@ open module org.cryptomator.cli {
     requires org.cryptomator.frontend.fuse;
     requires info.picocli;
     requires org.slf4j;
-    requires org.slf4j.jul;
     requires org.cryptomator.integrations.api;
+    requires org.fusesource.jansi;
 }
